9b8626f526cd0397ecdc302dd9e8d6516db3760a
Carl Verrier <carlitox@Carls-MacBook-Pro.local>
2023-09-27 17:47:25 -0400

    Scrolling and nav styles done

---
+++ src/Page/OurMIssion.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function OurMIssion() {
+  return (
+    <div>
+      OurMIssion
+      <Outlet />
+    </div>
+  );
+}
+
+export default OurMIssion;
