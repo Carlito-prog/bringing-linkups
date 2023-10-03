7b38e8c62d1f801454ed4fcb785c5c357bb05ee1
Carl Verrier <carlitox@Carls-MacBook-Pro.local>
2023-10-03 15:03:29 -0400

    Added button for contacting

--- src/Page/OurMIssion.tsx
+++ src/Page/OurMIssion.tsx
@@ -2,6 +2,12 @@ import { TextComponent } from "../components/TextComponent";
 import { ImageComponent } from "../components/ImageComponent";
 
 function OurMission() {
+  const handleClick = () => {
+    const element = document.getElementById("contact");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <section id="our-mission" className="darkBG">
       <div className="col-section two-col">
@@ -12,6 +18,10 @@ function OurMission() {
           }
         />
         <ImageComponent url={"src/assets/images/blu-logo.png"} alt={"text"} />
+        <div className="contactEarly">
+          <h5>Want To Join Our Efforts?</h5>
+          <button onClick={handleClick}>Join Us</button>
+        </div>
       </div>
     </section>
   );
