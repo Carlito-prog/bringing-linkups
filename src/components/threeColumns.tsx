d2475f6bcac4362d0ae4d331fa6058392788ee57
Carl Verrier <carlitox@Carls-MacBook-Pro.local>
2023-10-05 16:22:11 -0400

    use of memorization

--- src/components/threeColumns.tsx
+++ src/components/threeColumns.tsx
@@ -1,35 +1,33 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 type columnProps = {
   url: string;
   labelText: string;
   descriptionText: string;
-}
+};
 
 type ThreeColumnsProps = {
   componentTitle: string;
   columnData: columnProps[];
-}
+};
 
-export const ThreeColumns:React.FC<ThreeColumnsProps> = ({
+export const ThreeColumns: React.FC<ThreeColumnsProps> = ({
   componentTitle,
   columnData,
 }) => {
+  const theColumnData = useMemo(() => columnData, [columnData]);
   return (
     <div className="three-col-wrapper">
       <h2>{componentTitle}</h2>
       <div className="three-col-inner">
-        {columnData.map((data) => (
+        {theColumnData.map((data) => (
           <div key={Math.random()} className="col">
-              <img
-                src={data.url}
-                alt={data.labelText}
-              />
+            <img src={data.url} alt={data.labelText} />
             <h3>{data.labelText}</h3>
             <p>{data.descriptionText}</p>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
