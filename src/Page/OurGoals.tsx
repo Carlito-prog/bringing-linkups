54e0b006bd6f85da1db089bee937255e52db0c12
Carl Verrier <carlitox@Carls-MacBook-Pro.local>
2023-10-15 20:53:56 -0400

    add video

--- src/Page/OurGoals.tsx
+++ src/Page/OurGoals.tsx
@@ -2,7 +2,7 @@ export const OurGoals = () => {
   return (
     <section id="ourGoals darkBG" className="ourGoals">
       <video
-        src="https://firebasestorage.googleapis.com/v0/b/bringing-link-ups.appspot.com/o/FounderVIDSITE.mp4?alt=media&token=e28c9670-1404-4940-9a7a-d0a034224627&_gl=1*gebqur*_ga*MjA5MDUxODA4Ny4xNjk2Mjk5ODE1*_ga_CW55HF8NVT*MTY5Njk1OTUwNS4xNS4xLjE2OTY5NTk3NTQuNjAuMC4w"
+        src="https://firebasestorage.googleapis.com/v0/b/bringing-link-ups.appspot.com/o/FounderVIDSITE%20(1).mp4?alt=media&token=d144dcd6-cb1d-4250-bf01-0f0821fb825d&_gl=1*rq1ddt*_ga*MjA5MDUxODA4Ny4xNjk2Mjk5ODE1*_ga_CW55HF8NVT*MTY5NzQxNDk5Ni4yNC4xLjE2OTc0MTc1ODIuMjkuMC4w"
         className="solutionVideo"
         controls
         loop
