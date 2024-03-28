d2be78e13096fba82179aa8dcf235fea37b6fb2a
Carl Verrier <carlitox@Carls-MacBook-Pro.local>
2024-03-28 16:06:22 -0400

    added the better video

--- src/Page/OurGoals.tsx
+++ src/Page/OurGoals.tsx
@@ -2,10 +2,9 @@ export const OurGoals = () => {
   return (
     <section id="ourGoals" className="darkBG ourGoals">
       <video
-        src="https://firebasestorage.googleapis.com/v0/b/bringinglinkups.appspot.com/o/invideo-ai-1080%20BLU_%20Your%20Gateway%20to%20Real%20Connections%202024-03-28.mp4?alt=media&token=b207d9f9-eaca-4cee-9ba2-853540618cd9"
+        src="https://firebasestorage.googleapis.com/v0/b/bringinglinkups.appspot.com/o/BLU_Intro_1.mp4?alt=media&token=650291c8-ac0a-4240-910c-5a91b619cfe9"
         className="solutionVideo"
         controls
-        autoPlay
         loop
         width={"92%"}
       />
