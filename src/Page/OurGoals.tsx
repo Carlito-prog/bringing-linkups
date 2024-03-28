4b1cbc0cc5218150f08dc8f7a415649d03f711f6
Carl Verrier <carlitox@Carls-MacBook-Pro.local>
2024-03-28 15:52:20 -0400

    new video uploaded

--- src/Page/OurGoals.tsx
+++ src/Page/OurGoals.tsx
@@ -2,7 +2,7 @@ export const OurGoals = () => {
   return (
     <section id="ourGoals" className="darkBG ourGoals">
       <video
-        src="https://firebasestorage.googleapis.com/v0/b/bringinglinkups.appspot.com/o/BLU-Intro-Vid.mp4?alt=media&token=85ac93ea-970e-4ebd-a6c0-63c829e980a2"
+        src="https://firebasestorage.googleapis.com/v0/b/bringinglinkups.appspot.com/o/invideo-ai-1080%20BLU_%20Your%20Gateway%20to%20Real%20Connections%202024-03-28.mp4?alt=media&token=b207d9f9-eaca-4cee-9ba2-853540618cd9"
         className="solutionVideo"
         controls
         autoPlay
