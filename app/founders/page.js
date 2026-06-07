66dbd98f327d4c2c117246f9cc5370ce5c7deb22
ArNisha Henderson <arnisha34@yahoo.com>
2026-06-07 12:26:48 -0400

    added full width to founders grid

--- app/founders/page.js
+++ app/founders/page.js
@@ -36,7 +36,7 @@ export default function Team() {
       </div>
 
       <motion.div
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 h-full"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 h-full w-full"
         initial="hidden"
         animate={loaded ? "show" : "hidden"}
         variants={{
